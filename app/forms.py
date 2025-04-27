c0ae73ae573e8c82154e216d81e8fcfe36bd1367
Rafael Santos <66923351+RafaelSantos84@users.noreply.github.com>
2025-04-27 19:20:24 -0300

    correcao de login

--- app/forms.py
+++ app/forms.py
@@ -28,3 +28,10 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired()])
     submit = SubmitField('Entrar')
+
+class CadastroUsuarioForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    tipo = SelectField('Tipo de Conta', choices=[('borracharia', 'Borracharia'), ('admin', 'Admin')])
+    submit = SubmitField('Cadastrar')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
-from .forms import  LoginForm
-from .models import Usuario
-from . import db
+from app.models import Usuario, db
+from app.forms import LoginForm
 
 main = Blueprint('main', __name__)
 
-# Rota de login
 @main.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -15,21 +13,29 @@ def login():
         if usuario and usuario.verificar_senha(form.senha.data):
             login_user(usuario)
             flash('Login realizado com sucesso!', 'success')
-            return redirect(url_for('main.dashboard'))
+            return redirect(url_for('main.home'))
         else:
-            flash('E-mail ou senha incorretos.', 'danger')
+            flash('Email ou senha incorretos.', 'danger')
     return render_template('login.html', form=form)
 
-# Rota protegida (painel do usuário)
-@main.route('/dashboard')
-@login_required
-def dashboard():
-    return f'Bem-vindo, {current_user.nome}! Aqui é seu painel.'
-
-# Rota de logout
 @main.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('Você saiu da sua conta.', 'info')
+    flash('Você saiu da conta.', 'info')
     return redirect(url_for('main.login'))
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = CadastroUsuarioForm()
+    if form.validate_on_submit():
+        novo_usuario = Usuario(
+            nome=form.nome.data,
+            email=form.email.data,
+            tipo=form.tipo.data
+        )
+        novo_usuario.set_senha(form.senha.data)  # Criptografa a senha
+        db.session.add(novo_usuario)
+        db.session.commit()
+        return redirect(url_for('main.login'))  # Manda pro login depois de cadastrar
+    return render_template('register.html', form=form)
